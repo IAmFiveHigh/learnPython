38d22b8b252ee28fd59c747eec3e471154c0835d
five_high <z393533493@163.com>
2018-06-28 12:56:56 +0800

    新增文件自动根据格式分类 和分类后文件夹文件自动打乱放在一起

--- 文件自动归类/index.py
+++ 文件自动归类/index.py
@@ -1,2 +1,29 @@
 import shutil
+import os
 
+
+# 从分类里移动出来
+def move_to_father(path_name):
+    files = os.listdir("./" + path_name)
+    for f in files:
+        if not f.endswith(".DS_Store"):
+            for sub_f in os.listdir("./" + path_name + "/" + f):
+                shutil.move("./" + path_name + "/" + f + "/" + sub_f, "./" + path_name)
+
+
+# 分类
+def put_in_order(path_name):
+    files = os.listdir(path_name)
+    for f in files:
+        if len(f.split(".")) == 2:
+            folder_name = path_name + "/" + f.split(".")[-1]
+
+            if os.path.exists(folder_name):
+                shutil.move(path_name + "/" + f, folder_name)
+            else:
+                os.makedirs(folder_name)
+                shutil.move(path_name + "/" + f, folder_name)
+
+
+put_in_order("./problem2_files")
+# move_to_father("problem2_files")
