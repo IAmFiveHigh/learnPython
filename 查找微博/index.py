c9538e91bbd7d2bb42a6ef091c61a6d0e7c8c8d8
five_high <z393533493@163.com>
2018-07-24 18:56:51 +0800

    实现查找内容并csv格式保存本地

--- 查找微博/index.py
+++ 查找微博/index.py
@@ -29,10 +29,27 @@ def scroll_down():
 
 
 def get_content():
-    content = driver.find_elements_by_css_selector('div.WB_text')
+    content = driver.find_elements_by_css_selector('div.WB_detail')
+    list = []
     for i in content:
-        print(i.text)
+        date = i.find_element_by_css_selector('div.WB_from > a.S_txt2').text
+        text = i.find_element_by_css_selector('div.WB_text').text
+        l = [date, text]
+        list.append(l)
 
+    return list
+
+
+def save(list, name):
+    full_path = f'./{name}.csv'
+    if os.path.exists(full_path):
+        with open(full_path, 'a') as f:
+            writer = csv.writer(f)
+            writer.writerows(list)
+    else:
+        with open(full_path, 'w+') as f:
+            whiter = csv.writer(f)
+            whiter.writerows(list)
 
 url = 'https://weibo.com/starcraft?refer_flag=0000015010_&from=feed&loc=nickname&is_all=1'
 
@@ -44,7 +61,11 @@ time.sleep(5)
 # 翻到底
 scroll_down()
 
-get_content()
+# 获取内容
+content = get_content()
+print(content)
+save(content, "星际争霸的微博")
+
 
 # 获取下一页
 # next = driver.find_element_by_css_selector('a.page.next')
