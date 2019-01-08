c9476a3c863a45171edbaf7871c41f15404d4198
IAm5High <745170149@qq.com>
2019-01-08 14:34:56 +0800

    修改抓取数据返回结构 用list存储顺序

--- 抓取京东数据/index.py
+++ 抓取京东数据/index.py
@@ -2,8 +2,10 @@ from selenium import webdriver
 import time
 import json
 
-sku = '5089253'
-url = f'https://item.jd.com/{sku}.html'
+str = """100001332626
+    33743197985"""
+
+l = str.split('\n')
 
 
 def start_chrome():
@@ -18,16 +20,6 @@ def find_strangers():
     return el
 
 
-driver = start_chrome()
-driver.get(url)
-
-text = find_strangers()
-for i in text:
-    if i.text == "规格与包装":
-        i.click()
-        time.sleep(2)
-
-
 def find_status():
     btn_sel = 'div.Ptable-item'
     el = driver.find_elements_by_css_selector(btn_sel)
@@ -40,28 +32,51 @@ def find_name():
     return el.text
 
 
-item = find_status()
-d = {}
-for i in item:
+driver = start_chrome()
+for i in l:
+    sku = i
+    url = f'https://item.jd.com/{sku}.html'
+    
+    driver.get(url)
+    
+    text = find_strangers()
+    for i in text:
+        if i.text == "规格与包装":
+            i.click()
+            time.sleep(2)
 
+item = find_status()
+d = []
+for i in item:
+    
     h3 = i.find_element_by_css_selector('h3')
     print(h3.text)
-
+    # Ptable - tips
     dts = i.find_elements_by_css_selector('dl > dt')
-    dds = i.find_elements_by_css_selector('dl > dd')
+        
+        temp = i.find_element_by_css_selector('dl')
+        # dds = temp.find_elements_by_xpath('//dd[not(contains(@class, "Ptable - tips"))]')
+        dds = i.find_elements_by_css_selector('dl > dd')
+        dds_not = temp.find_elements_by_class_name("Ptable-tips")
+        
+        for dd in dds:
+            if dd in dds_not:
+                dds.remove(dd)
+        subd = []
+        for index in range(len(dts)):
+            print(dts[index].text, dds[index].text)
+            sub_result = {"key": dts[index].text, "value": dds[index].text}
+            subd.append(sub_result)
 
-    subd = {}
-    for index in range(len(dts)):
-        print(dts[index].text, dds[index].text)
-        subd[dts[index].text] = dds[index].text
-
-    array = [subd]
-
-    d[h3.text] = array
+    dic = {'h1': h3.text, 'list': subd}
+        d.append(dic)
 
 data = json.dumps(d).encode('utf-8').decode('unicode_escape')
 
-name = find_name()
+name = find_name().strip()[0:20]
+print(name)
 fileObject = open(f'{name}.txt', 'w')
 fileObject.write(data)
 fileObject.close()
+
+time.sleep(4)
